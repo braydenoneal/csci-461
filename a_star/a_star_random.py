bd00bcd566563517b84784828ed45dc9de8060af
Brayden O'Neal <braydenioneal@gmail.com>
2024-02-02 11:02:32 -0600

    Add random maze traversal with backtracking

---
+++ a_star/a_star_random.py
@@ -0,0 +1,144 @@
+import math
+import random
+from dataclasses import dataclass
+
+from graphics import *
+
+
+@dataclass
+class Node:
+    position: tuple[float, float]
+    adjacent_node_indices: list[int]
+
+
+nodes = []
+
+size = 20
+
+for y in range(size):
+    for x in range(size):
+        adjacent_node_indices = []
+
+        if x - 1 >= 0 and random.choice([False, True]):
+            adjacent_node_indices.append(y * size + x - 1)
+
+        if x + 1 < size and random.choice([False, True]):
+            adjacent_node_indices.append(y * size + x + 1)
+
+        if y - 1 >= 0 and random.choice([False, True]):
+            adjacent_node_indices.append((y - 1) * size + x)
+
+        if y + 1 < size and random.choice([False, True]):
+            adjacent_node_indices.append((y + 1) * size + x)
+
+        nodes.append(Node((x * 0.4, y * 0.4), adjacent_node_indices))
+
+for node_index, node in enumerate(nodes):
+    for adjacent_node_index in node.adjacent_node_indices:
+        nodes[adjacent_node_index].adjacent_node_indices.append(node_index)
+
+window_width = 720
+window_height = 640
+
+window = GraphWin('A*', window_width, window_height)
+
+window_scaling = 71
+window_padding = 64
+
+lines = {}
+
+
+def point_at_node_position(position: tuple[float, float]) -> Point:
+    x_position = position[0] * window_scaling + window_padding
+    y_position = position[1] * window_scaling + window_padding
+
+    return Point(x_position, y_position)
+
+
+def draw_circle(node: Node):
+    center_point = point_at_node_position(node.position)
+
+    circle = Circle(center_point, 16)
+    circle.setFill('white')
+    circle.setWidth(2)
+    circle.draw(window)
+
+    text = Text(center_point, nodes.index(node) + 1)
+    text.draw(window)
+
+
+def draw_line(node: Node, adjacent_node: Node, color='black'):
+    if f'{(node, adjacent_node)}' in lines:
+        lines[f'{(node, adjacent_node)}'].setFill(color)
+    elif f'{(adjacent_node, node)}' in lines:
+        lines[f'{(adjacent_node, node)}'].setFill(color)
+    else:
+        line = Line(point_at_node_position(node.position), point_at_node_position(adjacent_node.position))
+        line.setWidth(2)
+        line.setFill(color)
+        line.draw(window)
+
+        lines[f'{(node, adjacent_node)}'] = line
+
+
+for node in nodes:
+    adjacent_nodes = [nodes[adjacent_node_index] for adjacent_node_index in node.adjacent_node_indices]
+
+    for adjacent_node in adjacent_nodes:
+        draw_line(node, adjacent_node)
+
+
+@dataclass
+class Path:
+    node: Node
+    distance_to_adjacent_node: float
+    distance_to_end_node: float
+
+
+def get_best_path(paths, current_total_cost) -> Path:
+    return sorted(paths, key=lambda x: x.distance_to_adjacent_node + x.distance_to_end_node + current_total_cost)[0]
+
+
+start_node = nodes[0]
+end_node = nodes[-1]
+
+path: list[Node] = []
+visited = []
+
+current_node = start_node
+current_total_cost = 0
+
+while current_node is not end_node:
+    if current_node not in path:
+        path.append(current_node)
+
+    adjacent_nodes = [nodes[adjacent_node_index] for adjacent_node_index in current_node.adjacent_node_indices]
+    paths = []
+
+    for adjacent_node in adjacent_nodes:
+        if adjacent_node not in path and adjacent_node not in visited:
+            draw_line(current_node, adjacent_node, 'green')
+            time.sleep(0.01)
+            draw_line(current_node, adjacent_node)
+
+            distance_to_adjacent_node = math.dist(current_node.position, adjacent_node.position)
+            distance_to_end_node = math.dist(adjacent_node.position, end_node.position)
+
+            paths.append(Path(adjacent_node, distance_to_adjacent_node, distance_to_end_node))
+
+    if paths:
+        best_path = get_best_path(paths, current_total_cost)
+        current_total_cost += best_path.distance_to_adjacent_node
+
+        draw_line(current_node, best_path.node, 'red')
+
+        current_node = best_path.node
+    else:
+        last_node = path[-1]
+        current_node = path[-2]
+        visited.append(last_node)
+        path.pop()
+        draw_line(current_node, last_node, 'blue')
+
+window.getMouse()
+window.close()
