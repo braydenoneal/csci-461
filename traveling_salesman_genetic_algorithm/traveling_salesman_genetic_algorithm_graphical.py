6579af4fbbc281d584240d513ac4589c53f72139
Brayden O'Neal <braydenioneal@gmail.com>
2024-02-23 11:25:59 -0600

    Display iterations until they reach the best possible distance

--- traveling_salesman_genetic_algorithm/traveling_salesman_genetic_algorithm_graphical.py
+++ traveling_salesman_genetic_algorithm/traveling_salesman_genetic_algorithm_graphical.py
@@ -16,16 +16,16 @@ canvas.pack(expand=True)
 root.eval('tk::PlaceWindow . center')
 
 population_iterations = 32
-population_size = 128
-iterations = 256
+population_size = 1000
+iterations = 4096
 mutation_chance = 0.08
 
 
 def fitness_of_permutation(permutation):
     total_distance = 0
 
-    for i in range(len(permutation) - 1):
-        total_distance += math.dist(positions[permutation[i]], positions[permutation[i + 1]])
+    for i in range(len(permutation)):
+        total_distance += math.dist(positions[permutation[i]], positions[permutation[(i + 1) % len(permutation)]])
 
     return total_distance
 
@@ -37,6 +37,9 @@ positions_size = len(positions)
 
 best_genes = []
 
+text = tk.Label(root, text='')
+text.place(x=16, y=16)
+
 for population_iteration in range(population_iterations):
     population = []
 
@@ -46,61 +49,64 @@ for population_iteration in range(population_iterations):
         population.append(order)
 
     for iteration in range(iterations):
-        population = sorted(population, key=lambda x: fitness_of_permutation(x))[:population_size // 2]
+        if fitness_of_permutation(population[0]) > 20:
+            new_population = []
 
-        new_population = []
+            random.shuffle(population)
 
-        random.shuffle(population)
+            for crossover in range(population_size // 2):
+                parents = (random.choice(population), random.choice(population))
 
-        child_iter = 0
+                split_position = random.randint(0, positions_size)
 
-        for crossover in range(population_size // 2):
-            parents = (random.choice(population), random.choice(population))
+                for parent in (parents, parents[::-1]):
+                    child = parent[0][:split_position]
+                    indices = []
 
-            split_position = random.randint(0, positions_size)
+                    for order in parent[0][split_position:]:
+                        indices.append(parent[1].index(order))
 
-            for parent in (parents, parents[::-1]):
-                child = parent[0][:split_position]
-                indices = []
+                    for order in sorted(indices):
+                        child.append(parent[1][order])
 
-                for order in parent[0][split_position:]:
-                    indices.append(parent[1].index(order))
+                    for gene in range(positions_size):
+                        if random.random() < mutation_chance * (gene + 1):
+                            index1 = random.randint(0, positions_size - 1)
+                            index2 = random.randint(0, positions_size - 1)
 
-                for order in sorted(indices):
-                    child.append(parent[1][order])
+                            temp = child[index1]
+                            child[index1] = child[index2]
+                            child[index2] = temp
+                        else:
+                            break
 
-                if random.random() < mutation_chance:
-                    index1 = random.randint(0, positions_size - 1)
-                    index2 = random.randint(0, positions_size - 1)
+                    new_population.append(child)
 
-                    temp = child[index1]
-                    child[index1] = child[index2]
-                    child[index2] = temp
+            display_size = 1
 
-                new_population.append(child)
+            for order in range(display_size):
+                for i in range(positions_size):
+                    position1 = positions[population[order][i]]
+                    position2 = positions[population[order][(i + 1) % positions_size]]
 
-                for i in range(positions_size - 1):
-                    position1 = positions[child[i]]
-                    position2 = positions[child[i + 1]]
+                    x_mod = order // round(math.sqrt(display_size))
+                    y_mod = order % round(math.sqrt(display_size))
 
-                    x_mod = child_iter // round(math.sqrt(population_size))
-                    y_mod = child_iter % round(math.sqrt(population_size))
+                    x1 = position1[0] * 10 + x_mod * 80 + 64
+                    y1 = position1[1] * 10 + y_mod * 80 + 64
 
-                    x1 = position1[0] * 10 + x_mod * 80
-                    y1 = position1[1] * 10 + y_mod * 80
-
-                    x2 = position2[0] * 10 + x_mod * 80
-                    y2 = position2[1] * 10 + y_mod * 80
+                    x2 = position2[0] * 10 + x_mod * 80 + 64
+                    y2 = position2[1] * 10 + y_mod * 80 + 64
 
                     value = hex(round(64 + 191 * (i / (positions_size - 1))))[2:].rjust(2, '0')
                     canvas.create_line(x1, y1, x2, y2, width=2, fill=f'#{value}{value}{value}')
 
-                child_iter += 1
+            population = sorted(new_population, key=lambda x: fitness_of_permutation(x))[:population_size // 2]
 
-        population = new_population
+            text.config(text=f'Best Distance: {str(round(fitness_of_permutation(population[0]), 2))}')
 
-        root.update()
-        canvas.delete('all')
+            root.update()
+            canvas.delete('all')
 
     population = sorted(population, key=lambda x: fitness_of_permutation(x))
 
